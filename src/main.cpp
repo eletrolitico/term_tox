53f821fe532c6b49198a5ef0ba27e06fb97a3a1a
Dilson Pieve <pievetp@gmail.com>
2021-12-07 22:02:05 -0300

    test

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,16 @@
 
 int main()
 {
-    TOX_ERR_NEW err_new;
-    Tox *tox = tox_new(NULL, &err_new);
-    if (err_new != TOX_ERR_NEW_OK)
-    {
-        fprintf(stderr, "tox_new failed with error code %d\n", err_new);
-        exit(1);
-    }
+    // TOX_ERR_NEW err_new;
+    // Tox *tox = tox_new(NULL, &err_new);
+    // if (err_new != TOX_ERR_NEW_OK)
+    // {
+    //     fprintf(stderr, "tox_new failed with error code %d\n", err_new);
+    //     exit(1);
+    // }
 
     initscr();
-    printw("Hello World !!!");
+    printw("Hello World 2!!!");
     refresh();
     getch();
     endwin();
