9a4878a7431f61146a856821fcfb24b4eddd0e2b
Dilson Pieve <pievetp@gmail.com>
2021-12-07 22:44:26 -0300

    menu starter

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 #include <tox/tox.h>
 #include <ncurses.h>
+#include <signal.h>
+#include <string>
+#include <vector>
+
+const std::vector<std::string> options = {"Add", "Del", "Contact", "Accept", "Help"};
+
+size_t UpdateMove(int move, size_t chosen)
+{
+    if (move == KEY_BACKSPACE)
+        exit(0);
+
+    switch(move)
+    {
+        case KEY_UP:
+            --chosen; 
+            if (chosen < 0) chosen = 0;
+            break;
+        case KEY_DOWN:
+            ++chosen;
+            if (chosen >= options.size()) chosen = options.size() - 1;
+            break;
+        default:
+            std::cout << "go up or down" << '\n';
+            break;
+    }
+
+    return chosen;
+}
 
 int main()
 {
@@ -12,10 +40,45 @@ int main()
     //     exit(1);
     // }
 
+    //Start
     initscr();
-    printw("Hello World 2!!!");
+    noecho();
+    cbreak();
+
+    //Screen size
+    int Width, Height;
+    getmaxyx(stdscr, Height, Width);  
+
+    //Window
+    WINDOW* menu = newwin(10, Width-10, Height-10, 5);
+    box(menu, 0, 0);
     refresh();
-    getch();
+    wrefresh(menu);
+
+    //Arrow key
+    keypad(menu, true);
+
+    int move{ 0 };
+    size_t chosen{ 0 };
+
+    while(true)
+    {
+        for (size_t i = 0; i < options.size(); ++i)
+        {
+            if (i == chosen)
+                wattron(menu, A_REVERSE);
+            mvwprintw(menu, i + 1, 1, options[i].c_str());
+            wattroff(menu, A_REVERSE);
+        }
+
+        move = wgetch(menu);
+        chosen = UpdateMove(move, chosen);
+        
+        //printw("Option chosen: %s", options[chosen].c_str());
+    }
+
+    printw("Option chosen: %s", options[chosen].c_str());
+    refresh();
     endwin();
 
     return 0;
