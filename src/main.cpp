de8c6903e7693f57027c9a7062be2e8e3212e4e7
Dilson Pieve <pievetp@gmail.com>
2021-12-09 18:36:23 -0300

    add a draw_borders func

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,29 @@ void StartNCurses()
     curs_set(0);
 }
 
+void draw_borders(WINDOW* win, int start_y, int start_x, int end_y, int end_x) 
+{ 
+    // 4 corners 
+    mvwprintw(win, start_y, start_x, "+"); 
+    mvwprintw(win, end_y - 1, start_x, "+"); 
+    mvwprintw(win, start_y, end_x - 1, "+"); 
+    mvwprintw(win, end_y - 1, end_x - 1, "+"); 
+    
+    // sides 
+    for (int i = start_y + 1; i < (end_y - 1); i++) 
+    { 
+        mvwprintw(win, i, start_x, "|"); 
+        mvwprintw(win, i, end_x - 1, "|"); 
+    } 
+    
+    // top and bottom 
+    for (int i = start_x; i < (end_x - 1); i++) 
+    { 
+        mvwprintw(win, start_y, i, "-"); 
+        mvwprintw(win, end_y - 1, i, "-"); 
+    } 
+}
+
 int main()
 {   
     StartNCurses();
@@ -24,19 +47,22 @@ int main()
     getmaxyx(stdscr, yMax, xMax);  
 
     //Window
-    WINDOW* win = newwin(yMax / 2, xMax / 4, yMax / 4, 0);
-    box(win, 0, 0);
-    
+    WINDOW* win = newwin(yMax, xMax, 0, 0);
+    //box(win, 0, 0);
+
+    draw_borders(win, 0, 0, yMax / 6, xMax / 2);
+    draw_borders(win, yMax / 8, 0, yMax / 2, xMax / 2);
+
     //Arrow key
     keypad(win, true);
 
     std::vector<ncurses::Menu> menus
     {
-        ncurses::Menu(0, 2, "Options"), // title
-        ncurses::Menu(2, 2, "Add"),
-        ncurses::Menu(4, 2, "Del"),
-        ncurses::Menu(6, 2, "Invite"),
-        ncurses::Menu(8, 2, "Contact")
+        ncurses::Menu(0, 1, "Menu"), // title
+        ncurses::Menu(yMax / 16, 5, "Add"),
+        ncurses::Menu(yMax / 16, 9, "Del"),
+        ncurses::Menu(yMax / 16, 13, "Invite"),
+        ncurses::Menu(yMax / 16, 21, "Contact")
     };
 
     ncurses::MenuBar menu_bar{win, menus};
@@ -48,12 +74,12 @@ int main()
     {
         switch(ch)
         {
-            case KEY_UP:
+            case KEY_LEFT:
                 if (selected_menu > 1)
                     --selected_menu;
             break;
 
-            case KEY_DOWN:
+            case KEY_RIGHT:
                 if (selected_menu + 1 < menus.size())
                     ++selected_menu;
             break;
